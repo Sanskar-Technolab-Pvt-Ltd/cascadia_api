d90bb58073aba16692584829463a3c2a4e56693f
malav8 <malav@sanskartechnolab.com>
2025-04-21 17:55:25 +0530

    feat: api to filter roles based on custom query

---
+++ api/api.py
@@ -0,0 +1,10 @@
+import frappe
+
+@frappe.whitelist()
+def get_system_manager_users(doctype, txt, searchfield, start, page_len, filters):
+	return frappe.db.sql("""
+		select u.name, concat(u.first_name, ' ', u.last_name)
+		from tabUser u, `tabHas Role` r
+		where u.name = r.parent and r.role = 'Project Manager' 
+		and u.enabled = 1 and u.name like %s
+	""", ("%" + txt + "%"))
