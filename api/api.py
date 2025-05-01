0dcd82ab4a641ef9b2cc30881f2ff1888f78f81d
Abhiraaj R C <abhiraaj@sanskartechnolab.com>
2025-05-01 16:39:38 +0530

    feat: get-linked-team-members-api

--- api/api.py
+++ api/api.py
@@ -8,3 +8,23 @@ def get_system_manager_users(doctype, txt, searchfield, start, page_len, filters
 		where u.name = r.parent and r.role = 'Projects Manager' 
 		and u.enabled = 1 and u.name like %s
 	""", ("%" + txt + "%"))
+
+
+@frappe.whitelist()
+def get_linked_team_members(doctype, txt, searchfield, start, page_len, filters):
+	project = filters.get("project")
+	if not project:
+		return []
+
+	return frappe.db.sql("""
+        SELECT user, user
+        FROM `tabLinked Team Member 2`
+        WHERE parent = %(project)s
+        AND user LIKE %(txt)s
+        LIMIT %(start)s, %(page_len)s
+    """, {
+        "project": project,
+        "txt": f"%{txt}%",
+        "start": start,
+        "page_len": page_len
+    })
